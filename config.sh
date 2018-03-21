3a82db74d9e172ff356b387073cfb3ec53fa3d20
Matthew Brett <matthew.brett@gmail.com>
2018-03-21 17:58:04 +0000

    Drip dipnost - does it work for multiprocessing?

--- config.sh
+++ config.sh
@@ -10,5 +10,6 @@ function pre_build {
 function run_tests {
     # Runs tests on installed distribution from an empty directory
     python --version
-    python ../dipy/tools/dipnost dipy
+    echo "backend : agg" > matplotlibrc
+    nosetests --with-doctest --verbose dipy
 }
